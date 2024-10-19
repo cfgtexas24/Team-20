64218adea9bf0395146fa79e0438503e4d3f17c1
Ephraim <ephraim888sun@gmail.com>
2024-10-19 08:10:59 -0500

    add images for final product

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Award } from 'lucide-react'
+import { Award, Menu } from 'lucide-react'
 import { useUser } from '@clerk/nextjs'
 import { usePoints } from '@/hooks/usePoints'
 
@@ -18,15 +18,23 @@ const Navbar = () => {
   }, [isLoaded, isSignedIn, user, setPoints])
 
   return (
-    <div className='pl-2 pr-10 py-4 justify-between items-center inline-flex w-full bg-white shadow-sm bg-gradient-to-r from-blue-500 to-purple-600 p-6 mb-8 text-white'>
-      <div className='grow shrink basis-0 h-10 px-4 justify-start items-center gap-2 flex'>
-        <Link href='/'>
-          <Image src={'/logo.png'} alt='logo' width={150} height={50} />
-        </Link>
-      </div>
-      <div className='grow shrink basis-0 h-14 px-20 justify-start items-center gap-2 flex'></div>
-      <div className='justify-end items-center gap-2 flex'>
-        <Link href='/rewards' passHref>
+    <nav className='flex flex-row w-full bg-gradient-to-r from-blue-500 to-purple-600 p-4 text-white '>
+      <div className='w-full mx-auto flex justify-between items-center'>
+        <div className='flex items-center'>
+          <button className='mr-2 md:hidden'>
+            <Menu size={24} />
+          </button>
+          <Link href='/'>
+            <Image
+              src='/logo.png'
+              alt='logo'
+              width={120}
+              height={40}
+              className='max-h-8 w-auto'
+            />
+          </Link>
+        </div>
+        <Link href='/rewards' className='flex items-center'>
           <div className='px-3 py-2 rounded-full bg-yellow-100 hover:bg-yellow-200 transition-colors duration-200 flex items-center gap-2 cursor-pointer'>
             <Award className='w-5 h-5 text-yellow-600' />
             <div className="text-yellow-700 text-sm font-semibold font-['Inter']">
@@ -35,7 +43,7 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
-    </div>
+    </nav>
   )
 }
 
