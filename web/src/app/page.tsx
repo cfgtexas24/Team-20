6e00f504309d3552dd79740b13566a023c1e01f2
blake-lee-38 <blake_lee38@yahoo.com>
2024-10-18 21:21:00 -0500

    Added google maps

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,23 @@
+'use client'
+
 import EventsPage from '@/components/events'
 import YoutubeVideos from '@/components/youtube-videos'
+import PlaceComponent from './Autocomplete.jsx';
 
-export default function Home() {
+const MyPage = () => {
   return (
+    <>
     <div className='w-full'>
       {/* <h1>Welcome to YouTube Video Fetcher</h1> */}
       {/* <YoutubeVideos /> */}
       <EventsPage />
     </div>
-  )
-}
+    <div>
+      <p>My Page</p>
+      <PlaceComponent />
+    </div>
+    </>
+  );
+};
+
+export default MyPage;
